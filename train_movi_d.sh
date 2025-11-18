1cb604054c7bbf71e650845b5bdcbc32c6e79ecc
skyworker-sinica <luke.yc.yang@gmail.com>
2025-11-18 08:46:38 +0800

    modify bash script

--- train_movi_d.sh
+++ train_movi_d.sh
@@ -6,7 +6,13 @@ OUT_DIR=/home/workspace/uvos/result/
 # export CUDA_VISIBLE_DEVICES=2
 # export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
+# Go to steve repo
+echo 'Enter STEVE repo...'
+cd ./workspace/uvos/steve/
+$ pwd
+
 # Train Movi-D
+echo 'Start pre-training STEVE on Movi-d'
 python train.py \
     --data_path $DATA_DIR/movi/d \
     --log_path $OUT_DIR/movi/d \
