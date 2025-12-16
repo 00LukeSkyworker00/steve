8d348c5096c8fcbceb80bf51e83692edf0688a8d
skyworker-sinica <luke.yc.yang@gmail.com>
2025-12-16 14:34:41 +0800

    add movi a

---
+++ train_movi_a.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DATA_DIR=/work/skyworker0/data/uvos
+OUT_DIR=/home/skyworker0/workspace/uvos/result
+
+# Install packages
+pip install moviepy
+pip install wandb
+pip install --upgrade pydantic==2.7.0
+
+# Setup WanDB (put your api key in a txt file in root.)
+export WANDB_API_KEY=$(cat .wandb.txt)
+
+# Go to steve repo
+echo 'Enter STEVE repo...'
+cd ./workspace/uvos/steve/
+
+# Train
+echo 'Start pre-training STEVE on Movi-a'
+python -u train.py \
+    --dataset movi_a \
+    --data_path $DATA_DIR/movi/a \
+    --out_path $OUT_DIR/movi/a \
+    --num_slots 12 \
+    --steps 200000 \
+    --use_dp \
